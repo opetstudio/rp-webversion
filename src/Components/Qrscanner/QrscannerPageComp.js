4126ee1fb2d33676220614943f6f87f6f793e16b
Nofrets Poai <opetstudio@gmail.com>
2019-09-17 19:40:12 +0700

    qr scanner ok

--- src/Components/Qrscanner/QrscannerPageComp.js
+++ src/Components/Qrscanner/QrscannerPageComp.js
@@ -17,9 +17,26 @@ import {
   Card
 } from 'semantic-ui-react'
 import { FormattedMessage } from 'react-intl'
+import QrReader from 'react-qr-reader'
 export default class QrscannerPageComp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      result: null
+    }
+  }
   handleSubmit () {
 
+  }
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
   }
   render () {
     return (
@@ -44,12 +61,27 @@ export default class QrscannerPageComp extends Component {
                   <Form loading={this.props.isRequesting}>
                     <Form.Group widths='equal'>
                       <Form.Field>
-                        <label>{(<FormattedMessage id='label.userid' />)}</label>
-                        <Input type='text' name='userid' fluid placeholder={this.props.intl.formatMessage({ id: 'label.userid' })} icon={<Icon name='lock' />} onChange={this.handleChange} />
+                        <label>{(<FormattedMessage id='label.transactionid' />)}</label>
+                        <Input type='text' name='transactionid' fluid placeholder={this.props.intl.formatMessage({ id: 'label.transactionid' })} icon={<Icon name='lock' />} onChange={this.handleChange} />
                       </Form.Field>
                     </Form.Group>
-                    <Button primary onClick={this.handleSubmit} disabled={(this.props.isRequesting)}>{(<FormattedMessage id='label.generate' />)}</Button>
+                    {this.state.result && (<Form.Group widths='equal'>
+                      <Form.Field>
+                        <label>{(<FormattedMessage id='label.pin' />)}</label>
+                        <Input type='text' name='pin' fluid placeholder={this.props.intl.formatMessage({ id: 'label.pin' })} icon={<Icon name='lock' />} onChange={this.handleChange} />
+                      </Form.Field>
+                    </Form.Group>)}
+                    <Button primary onClick={this.handleSubmit} disabled={(this.props.isRequesting)}>{(<FormattedMessage id='label.submit_transaction' />)}</Button>
                   </Form>
+                  {/* <p>{this.state.result}</p> */}
+                </Grid.Column>
+                <Grid.Column>
+                  {this.state.result === null && (<QrReader
+                    delay={300}
+                    onError={this.handleError}
+                    onScan={this.handleScan}
+                    style={{ width: '100%' }}
+                  />)}
                 </Grid.Column>
               </Grid.Row>
             </Grid>
