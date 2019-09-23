a84b399aac34cf31c02045a931070e5323154cf2
Nofrets Poai <opetstudio@gmail.com>
2019-09-23 09:49:54 +0700

    Styling login

--- src/Components/Login/LoginPageComponent.js
+++ src/Components/Login/LoginPageComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
-import {Images} from '../../Themes'
+import {Images, Colors} from '../../Themes'
 
 const messageBox = (msg, success) => (
   <Message success={success} negative={!success}>
@@ -31,36 +31,44 @@ class LoginPageComponent extends Component {
     return false
   }
   render () {
-    return (<Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-          <Image src='/logo.png' /> Log-in to your account
-        </Header>
-        <Form size='large' onSubmit={(e, o) => this._formOnSubmit(e, o)}>
-          <Segment stacked>
-            {this.props.responseCode !== '' && this.props.responseCode === 'MBDD00' && messageBox(this.props.responseDescription, true)}
-            {this.props.responseCode !== '' && this.props.responseCode !== 'MBDD00' && messageBox(this.props.responseDescription, false)}
-            <Form.Input name='userid' onChange={this.handleChange} fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-            <Form.Input
-              name='password'
-              onChange={this.handleChange}
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
+    return (
+      <div style={{backgroundImage: 'url(' + Images.base_bg + ')', backgroundSize: '100% 100%',padding:'5%'}}>
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='center'>
+              <Grid.Column >
+                <Image verticalAlign='middle' src={Images.rp_logo_white}  style={{width:'80px',height:'80px'}}/>
+                <br/>
+                <p style={{color:Colors.white}}>Log-in to your account</p>
+              </Grid.Column>
+            </Header>
+            <Form size='large' onSubmit={(e, o) => this._formOnSubmit(e, o)}>
+              <Segment stacked>
+                {this.props.responseCode !== '' && this.props.responseCode === 'MBDD00' && messageBox(this.props.responseDescription, true)}
+                {this.props.responseCode !== '' && this.props.responseCode !== 'MBDD00' && messageBox(this.props.responseDescription, false)}
+                <Form.Input name='userid' onChange={this.handleChange} fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                <Form.Input
+                  name='password'
+                  onChange={this.handleChange}
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                />
 
-            <Button type='submit' color='teal' fluid size='large'>
+                <Button type='submit' style={{backgroundColor: Colors.rp_gray2}} color='teal' fluid size='large'>
             Login
-            </Button>
-          </Segment>
-        </Form>
-        <Message>
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
         New to us? <a href='#'>Sign Up</a>
-        </Message>
-      </Grid.Column>
-    </Grid>)
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
   }
 }
 export default LoginPageComponent
--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -6,6 +6,8 @@ const images = {
   mastercardicon: require('../Images/mastercard-icon.png'),
   pcidss: require('../Images/pcidss.png'),
   sslsecurity: require('../Images/sslsecurity.png'),
-  plinkpayment: require('../Images/plinkpayment.png')
+  plinkpayment: require('../Images/plinkpayment.png'),
+  base_bg: require('../Images/Bg-building.png'),
+  rp_logo_white: require('../Images/raya-pay-logo-white.png')
 }
 export default images
